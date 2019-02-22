db8abf00ef80431487930c08fe57fde545ad3e8e
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-02-21 21:46:19 -0500

    Added server.js and express installed according to next.js tutorial beggining

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 export default () => (
     <div>
       <p>This is the about page</p>
     </div>
   )
-=======
-import Layout from '../components/Layout.js'
-
-export default () => (
-  <Layout>
-    <p>This is the about page</p>
-  </Layout>
-)
->>>>>>> 3c90772f212037140699017e0e64f12fdde485ed
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from '../components/Header'
 
 export default () => (
@@ -9,60 +8,3 @@ export default () => (
 )
 
 //export default Index
-=======
-import Layout from '../components/Layout.js'
-import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
-
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-    <style jsx>{`
-      h1,
-      a {
-        font-family: 'Arial';
-      }
-
-      ul {
-        padding: 0;
-      }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </Layout>
-)
-
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-  const data = await res.json()
-
-  console.log(`Show data fetched. Count: ${data.length}`)
-
-  return {
-    shows: data
-  }
-}
-
-export default Index
->>>>>>> 3c90772f212037140699017e0e64f12fdde485ed
