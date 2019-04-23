2a6014731145b0e4f8bdd82dd113792ef70f8466
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-04-23 13:57:47 -0400

    Iinitial building, switching to new branch to isntall cross-env

---
+++ widgets/announcement/src/AnnouncementContent.js
@@ -0,0 +1,75 @@
+/**
+ * @fileoverview Slideshow component that given an array of slide descriptions
+ * of mixed types, renders the slides and automatically plays the slideshow for
+ * the given durations
+ */
+
+import React, { Component } from 'react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
+config.autoAddCss = false
+
+const DEFAULT_COLOR = '#34495e'
+const DEFAULT_TEXT_COLOR = '#ffffff'
+const DEFAULT_ACCENT_COLOR = '#A0A0A0'
+const DEFAULT_TEXT = 'You did not fill this out'
+
+class AnnouncementContent extends Component {
+  render() {
+    const {
+      data: {
+        text = DEFAULT_TEXT,
+        textColor = DEFAULT_TEXT_COLOR,
+        color = DEFAULT_COLOR,
+        accentColor = DEFAULT_ACCENT_COLOR
+      } = {}
+    } = this.props
+    return (
+      <div className='announce'>
+        <div className='background'>
+          <div>{'One'}</div>
+          <div>{'Two'}</div>
+          <div>{'Three'}</div>
+        </div>
+        <div className='text'>{text}</div>
+        <style jsx>
+          {`
+            .announce {
+              position: relative;
+              box-sizing: border-box;
+              height: 100%;
+              width: 100%;
+              background: ${color};
+              color: ${textColor};
+              flex: 1;
+              padding: 16px;
+              font-family: 'Open Sans', sans-serif;
+              display: flex;
+              flex-direction: row;
+              justify-content: center;
+              align-items: center;
+            }
+            .announce .background {
+              width: 100%;
+              height: 100%;
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 0;
+            }
+            .announce .text {
+              font-family: 'Open Sans', sans-serif;
+              font-size: 1.3em;
+              padding: 16px;
+              font-weight: 600;
+              text-align: center;
+              z-index: 1;
+            }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
+
+export default AnnouncementContent
