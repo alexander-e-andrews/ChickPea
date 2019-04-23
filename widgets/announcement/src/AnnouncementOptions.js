f93748e9b4c7cbf2643f0ae2e6a621495d239bef
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-04-23 13:57:47 -0400

    Iinitial building, switching to new branch to isntall cross-env

---
+++ widgets/announcement/src/AnnouncementOptions.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { Form, Input, InlineInputGroup } from '../../../components/Form'
+
+import AnnouncementContent from './AnnouncementContent'
+
+class AnnouncementOptions extends Component {
+  constructor(props) {
+    super(props)
+    const { text, color, textColor, accentColor} = props.data || {}
+    this.state = {
+      text,
+      color,
+      textColor,
+      accentColor
+    }
+  }
+  handleChange = (name, value) => {
+    const { onChange = () => {} } = this.props
+    this.setState(
+      {
+        [name]: value
+      },
+      () => {
+        onChange(this.state)
+      }
+    )
+  }
+
+  render() {
+    const { text, color, textColor, accentColor } = this.state
+    return (
+      <div className={'container'}>
+        <Form>
+          <h3>Widget: Announcement</h3>
+          <p>Choose your preferences for the announcement widget.</p>
+          <InlineInputGroup>
+            <Input
+              inline={false}
+              label={'Background color'}
+              type={'color'}
+              name={'color'}
+              value={color}
+              onChange={this.handleChange}
+            />
+            <Input
+              inline={false}
+              label={'Text color'}
+              type={'color'}
+              name={'textColor'}
+              value={textColor}
+              onChange={this.handleChange}
+            />
+            <Input
+              inline={false}
+              label={'Animation'}
+              type={'select'}
+              name={'animation'}
+              value={animation}
+              choices={[
+                { id: 'confetti', label: 'Confetti' },
+                { id: 'balloons', label: 'Balloons' }
+              ]}
+              onChange={this.handleChange}
+              expand={false}
+            />
+          </InlineInputGroup>
+
+          <Input
+            inline={false}
+            label={'Text to be displayed'}
+            type={'textarea'}
+            name={'text'}
+            value={text}
+            onChange={this.handleChange}
+          />
+        </Form>
+        <div className={'previewContainer'}>
+          <p>Preview</p>
+          <div className={'preview'}>
+            <AnnouncementContent data={this.state} />
+          </div>
+        </div>
+        <style jsx>
+          {`
+            h3,
+            p {
+              font-family: 'Open Sans', sans-serif;
+            }
+            .container {
+              display: flex;
+              flex-direction: row;
+            }
+            .preview {
+              display: block;
+              width: 240px;
+              height: 240px;
+              border-radius: 6px;
+              overflow: hidden;
+            }
+            .previewContainer {
+              margin-left: 16px;
+              width: 240px;
+            }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
+
+export default AnnouncementOptions
