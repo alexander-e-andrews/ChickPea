e190905d2d4d9e00aa8bf20efc3a52b699b116ee
Wassim Gharbi <wassgha@gmail.com>
2019-04-28 11:28:24 -0400

    Implements the Youtube widget

--- widgets/youtube/src/YoutubeOptions.js
+++ widgets/youtube/src/YoutubeOptions.js
@@ -52,7 +52,6 @@ class YoutubeOptions extends Component {
             type={'text'}
             name={'title'}
             value={title}
-            placeholder={'Optional title...'}
             onChange={this.handleChange}
           />
         </Form>
